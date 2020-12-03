aa1a285ff722b85b1f00bd56d09e6ea7d3ed818a
Élenn Milhomem <elenndypaulla@hotmail.com>
2020-12-03 20:44:48 -0300

    Início da criação das models

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model): 
+    primeiro_nome = models.CharField(max_length=30)
+    ultimo_nome = models.CharField(max_length=30)
+    cidade = models.CharField(max_length=30)
+    descricao_usuario = models.CharField (max_length=120)
+    pontuacao = models.IntegerField()
+    # usuario = models.ManyToManyField(Usuario)
+
+class Estabelecimento(models.Model):
+    nome_est = models.CharField(max_length=30)
+    endereco_est = models.CharField(max_length=120)
+    telefone_est = models.CharField(max_length=11) 
+    descricao_est = models.CharField(max_length=120)
+
+class Avaliacao(models.Model):
+    nota = models.IntegerField()
+    data = models.DateField()
+    comentario = models.CharField(max_length=240)
+    estabelecimento = models.ForeignKey(Estabelecimento, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
+
+class Categoria(models.Model):
+    nome_categoria = models.CharField(max_length=30)
+    nome_descricao = models.CharField(max_length=30)
+    usuario = models.ManyToManyField(Usuario)
+    estabelecimento = models.ManyToManyField(Estabelecimento)
+
+class Post(models.Model):
+    data = models.DateField()
+    imagem = models.ImageField()
+    comentario = models.CharField(max_length=240)      
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)    
+
+class Desconto(models.Model):
+    nome = models.CharField(max_length=30)
+    valor = models.FloatField()
+    voucher = models.ImageField()
+    estabelecimento = models.ForeignKey(Estabelecimento, on_delete=models.CASCADE)
+    usuario = models.ManyToManyField(Usuario)
+
+
